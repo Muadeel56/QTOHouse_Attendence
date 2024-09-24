81ccf05c1beb9c2941fd6f66ac361c8c0c3e463c
Muadeel56 <muadeel59@gmail.com>
2024-09-24 14:10:46 +0500

    Creating Sidebar for Dashboard

--- qtohouse-attendence/src/Components/Sidebar.jsx
+++ qtohouse-attendence/src/Components/Sidebar.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar'
-import logo from '../assets/images/logo.svg'
+import React from "react";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import logo from "../assets/images/logo.svg";
 
 function SidebarMenu() {
   return (
-        <Sidebar collapsedWidth={0}>
-            <div className='flex justify-center items-center my-6 '>
-                <img src={logo} alt="logo" className='w-[100px] '/>
-            </div>
-        
+    <Sidebar className="">
+      <div className="flex justify-center items-center my-6 ">
+        <img src={logo} alt="logo" className="w-[100px] " />
+      </div>
+
       <Menu>
         <SubMenu label="Dashboard">
-        <MenuItem>Home</MenuItem>
-        <MenuItem>Reviews</MenuItem>
+          <MenuItem>Charts</MenuItem>
+          <MenuItem>Lists</MenuItem>
         </SubMenu>
-        <MenuItem>Team</MenuItem>
-        <MenuItem>Contact</MenuItem>
+        <SubMenu label="Rules and Regulations">
+          <SubMenu label="Profile">
+            <MenuItem>Profile Page</MenuItem>
+            <MenuItem>Profile Page Settings</MenuItem>
+          </SubMenu>
+          <MenuItem>FAQs</MenuItem>
+          <MenuItem>Privacy Policy</MenuItem>
+          <MenuItem>Terms and Conditions</MenuItem>
+        </SubMenu>
+        <SubMenu label="Settings">
+            <SubMenu label="Attendance">
+                <MenuItem>Shifts</MenuItem>
+                <MenuItem>Salary</MenuItem>
+                <MenuItem>Holidays</MenuItem>
+                <MenuItem>Overtime</MenuItem>
+                <MenuItem>Profile Settings</MenuItem>
+            </SubMenu>
+            <MenuItem>Project</MenuItem>
+        </SubMenu>
       </Menu>
-    </Sidebar>    
-  )
+    </Sidebar>
+  );
 }
 
-export default SidebarMenu
+export default SidebarMenu;
